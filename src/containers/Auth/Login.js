02e6585a386e48093eafbc4bd52ae099166f6581
tuandung2109 <dinhtuandung10a6@gmail.com>
2025-05-10 00:59:38 +0700

    #33 Tạo Form Login Responsive Với React.js

---
+++ src/containers/Auth/Login.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { push } from "connected-react-router";
+import * as actions from "../../store/actions";
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+import './Login.scss';
+import { FormattedMessage } from 'react-intl';
+import { divide } from 'lodash';
+
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="login-background">
+                <div className="login-container">
+                    <div className="login-content row">
+                        <div className="col-12 text-login">Login</div>
+                        <div className="col-12 form-group login-input">
+                            <label>Username:</label>
+                            <input type="text" className="form-control" placeholder="Enter your username" />
+                        </div>
+                        <div className="col-12 form-group login-input">
+                            <label>Password:</label>
+                            <input type="text" className="form-control" placeholder="Enter your password" />
+                        </div>
+                        <div>
+                            <button className="col-12 btn-login">Login</button>
+                        </div>
+                        <div className="col-12">
+                            <span className="forgot-password">Forgot your password?</span>
+                        </div>
+                        <div className="col-12 text-center mt-3">
+                            <span className="text-other-login">Or login with:</span>
+                        </div>
+                        <div className="col-12 social-login">
+                            <i class="fa-brands fa-google-plus google"></i>
+                            <i class="fa-brands fa-facebook facebook"></i>
+                        </div>
+                        <div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        navigate: (path) => dispatch(push(path)),
+        adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
+        adminLoginFail: () => dispatch(actions.adminLoginFail()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
