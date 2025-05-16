441eae60d4cdd1effe0b4d86050a5983cb365c80
tuandung2109 <dinhtuandung10a6@gmail.com>
2025-05-16 22:57:00 +0700

    #47 Sử dụng Redux để chuyển đổi ngôn ngữ

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -3,11 +3,18 @@ import { connect } from 'react-redux';
 import './HomeHeader.scss'
 import logo from '../../assets/images/logo.svg'
 import { FormattedMessage } from 'react-intl';
-
+import { LANGUAGES } from '../../utils';
+import { changeLanguageApp } from '../../store/actions';
 
 class HomeHeader extends Component {
 
+
+    changeLanguage = (language) => {
+        this.props.changeLanguageAppRedux(language)
+    }
     render() {
+        let language = this.props.language;
+
         return (
             <React.Fragment>
                 <div className='home-header-container'>
@@ -39,8 +46,8 @@ class HomeHeader extends Component {
                             <div className='support'><i className="fa-solid fa-circle-question"></i> 
                                 <FormattedMessage id="homeheader.support"/>
                             </div>
-                            <div className='language-vi'> VN </div>
-                            <div className='language-en'> EN </div>
+                            <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}> <span onClick={() => this.changeLanguage(LANGUAGES.VI)}>VN</span> </div>
+                            <div className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}> <span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span> </div>
                         </div>                                        
                     </div>
                 </div>     
@@ -91,13 +98,13 @@ class HomeHeader extends Component {
 const mapStateToProps = state => {
     return {
         language: state.app.language,
-        
         isLoggedIn: state.user.isLoggedIn
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
     };
 };
 
