be81c64b72e34da950532f65471a82f591f45660
tuandung2109 <dinhtuandung10a6@gmail.com>
2025-05-17 18:13:48 +0700

    #49 Thao tác với slick-slider

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -7,43 +7,59 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css"; 
 
-class Specialty extends Component {
+// import specialtyImg from "../../../assets/specialty/test1.jpg";
 
+
+
+class Specialty extends Component {
     render() {
-        let settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
+            let settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 1,
         };
 
         return (
             <div className="section-specialty">
-                <div className="specialty-content">
-                    <Slider {...settings}>
-                        <div className='img-customize'>
-                            <h3>1</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>2</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>3</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>4</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>5</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>6</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>7</h3>
-                        </div>
-                    </Slider>
+                <div className="specialty-container">
+                    <div className='specialty-header'>
+                        <span className='title-section'>Chuyên khoa phổ biến</span>
+                        <button className='btn-section'>Xem thêm</button>
+                    </div>
+                    <div className='specialty-body'>
+                        <Slider {...settings}>
+                            <div className='specialty-customize'>
+                                <div className='bg-image'/>
+                                <div>Cơ xương khớp 1</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-image'/>
+                                <div>Cơ xương khớp 2</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-image'/>
+                                <div>Cơ xương khớp 3</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-image'/>
+                                <div>Cơ xương khớp 4</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-image'/>
+                                <div>Cơ xương khớp 5</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-image'/>
+                                <div>Cơ xương khớp 6</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-image'/>
+                                <div>Cơ xương khớp 7</div>
+                            </div>
+                        </Slider>                        
+                    </div>
                 </div>
             </div>
         );
